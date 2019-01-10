502b4047bca0f471a6b9159e221b372fcaebe373
DominicLutherEarl <DominicLutherEarl@users.noreply.github.com>
2019-01-10 18:18:46 +0530

    Update Requestheader.cs

--- aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Requestheader.cs
+++ aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Requestheader.cs
@@ -6,19 +6,19 @@ public class RequestDetails
         [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
         public string orderid { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
+        [Required(AllowEmptyStrings = false, ErrorMessage="ordertype field is not present")]
         public string ordertype { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
+        [Required(AllowEmptyStrings = false, ErrorMessage="businessId field is not present")]
         public string businessid { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
+        [Required(AllowEmptyStrings = false, ErrorMessage="matchtype field is not present")]
         public string matchtype { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
+        [Required(AllowEmptyStrings = false, ErrorMessage="noofrecords field is not present")]
         public string noofrecords { get; set; }
  
-        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
+
         public string email { get; set; }  
 
   }
