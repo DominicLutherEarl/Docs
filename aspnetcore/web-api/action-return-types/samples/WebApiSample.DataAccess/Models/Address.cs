82fe7817747e0c4b57ff4200623357badc442e67
DominicLutherEarl <DominicLutherEarl@users.noreply.github.com>
2019-01-10 18:44:51 +0530

    Create Address.cs

---
+++ aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Address.cs
@@ -0,0 +1,16 @@
+
+  namespace Mc.TD.Upload.Domain.DataMatch
+{
+    public class Address
+    {
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string address3 { get; set; }
+        public string address4 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public string zip { get; set; }
+    }
+}
+  
