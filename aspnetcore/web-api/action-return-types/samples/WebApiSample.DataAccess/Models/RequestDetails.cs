4635636268eec87eb5a2cad61a46fe6b30ec62cc
DominicLutherEarl <DominicLutherEarl@users.noreply.github.com>
2019-01-10 18:28:01 +0530

    Update RequestDetails.cs

--- aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/RequestDetails.cs
+++ aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/RequestDetails.cs
@@ -5,14 +5,31 @@ namespace Mc.TD.Upload.Domain.DataMatch
 {
     public class Requestdetail
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
         public string id { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
         public string requesttype { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
         public string trackid { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
         public string companyname { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
         public Address address { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
         public string phone { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
         public string url { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage="orderId field is not present")]
         public string contact { get; set; }
+        
+        
         public string ein { get; set; }
         public string tin { get; set; }
         public string vat { get; set; }
