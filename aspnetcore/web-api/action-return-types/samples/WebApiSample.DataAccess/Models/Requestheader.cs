93b4ec2e052cd4501e781305dcea72f688ab1660
DominicLutherEarl <DominicLutherEarl@users.noreply.github.com>
2019-01-10 18:49:13 +0530

    Update Requestheader.cs

--- aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Requestheader.cs
+++ aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Requestheader.cs
@@ -14,7 +14,7 @@ public class RequestDetails
         [Required(AllowEmptyStrings = false, ErrorMessage="businessId field is not present")]
         public string businessid { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage="matchtype field is not present")]
+
         public string matchtype { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage="noofrecords field is not present")]
