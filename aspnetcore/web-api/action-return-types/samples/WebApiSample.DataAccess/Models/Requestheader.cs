ec3ec88ca86bbab7dcfd1829079944a463f3eb3a
DominicLutherEarl <DominicLutherEarl@users.noreply.github.com>
2019-01-10 11:51:57 +0530

    Update Requestheader.cs

--- aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Requestheader.cs
+++ aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Requestheader.cs
@@ -1,1 +1,18 @@
-namespace vvv{}
+namespace MC.TD.Upload.Domain.DataMatch
+{
+public class RequestDetails
+  {
+  public string orderid { get; set; }
+
+        public string ordertype { get; set; }
+
+        public string businessid { get; set; }
+
+        public string matchtype { get; set; }
+
+        public string noofrecords { get; set; }
+
+        public string email { get; set; }  
+
+  }
+}
