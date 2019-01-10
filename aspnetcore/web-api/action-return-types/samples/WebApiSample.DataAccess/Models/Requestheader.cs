6f1c62950018e8c1c53c806cb0ea64a7b0a5ea1f
DominicLutherEarl <DominicLutherEarl@users.noreply.github.com>
2019-01-10 18:11:21 +0530

    Update Requestheader.cs

--- aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Requestheader.cs
+++ aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Requestheader.cs
@@ -6,17 +6,20 @@ public class RequestDetails
         [Required(AllowEmptyStrings = false)]
         public string orderid { get; set; }
 
-  [Required(AllowEmptyStrings = false)]
+        [Required(AllowEmptyStrings = false)]
         public string ordertype { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         public string businessid { get; set; }
 
-        public string matchtype { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string matchtype { get; set; }
 
-        public string noofrecords { get; set; }
-
-        public string email { get; set; }  
+        [Required(AllowEmptyStrings = false)]
+        public string noofrecords { get; set; }
+ 
+        [Required(AllowEmptyStrings = false)]
+        public string email { get; set; }  
 
   }
 }
