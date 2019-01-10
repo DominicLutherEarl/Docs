419e59d33dd3e8120b31d7321296bc481d1b2edc
DominicLutherEarl <DominicLutherEarl@users.noreply.github.com>
2019-01-10 18:35:36 +0530

    Create Linking.cs

---
+++ aspnetcore/web-api/action-return-types/samples/WebApiSample.DataAccess/Models/Linking.cs
@@ -0,0 +1,13 @@
+
+
+ 
+using System.Collections.Generic;
+
+namespace Mc.TD.Upload.Domain.DataMatch
+{
+    public class Linking
+    {
+        public string linktrackid { get; set; }
+        public IList<Linkcompliance> linkcompliance { get; set; }
+    }
+}
